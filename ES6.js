d0ddc1d0aab83fae33b517c1730f61f8049f231d
Lokesh Guntreddi <lokeshguntreddi58@gmail.com>
2024-10-11 20:49:24 +0530

    Create ES6.js
    basics of es6

---
+++ ES6.js
@@ -0,0 +1,13 @@
+let name1='lokesh' ;
+let name2='guntreddi' ;
+console.log(name1+ " "+ name2) ;
+//using template literars
+console.log(`${name1} ${name2}`) ;
+//terary operator 
+let myfood='rice';
+function Myfirstfood(foodname){
+    return foodname 
+}
+function Myfirstfood(foodname){
+    return foodname 
+}
